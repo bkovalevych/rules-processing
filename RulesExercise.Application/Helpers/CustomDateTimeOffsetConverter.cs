a2426238d6bab8ec0b35d2f8a44880ecd7f2e4b8
bohdan.kovalevych <bohdan.kovalevych@nure.ua>
2022-05-04 14:03:39 +0300

    Writed tests

---
+++ RulesExercise.Application/Helpers/CustomDateTimeOffsetConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RulesExercise.Application.Helpers
+{
+    public class CustomDateTimeOffsetConverter : JsonConverter<DateTimeOffset>
+	{
+		public override void Write(Utf8JsonWriter writer, DateTimeOffset date, JsonSerializerOptions options)
+		{
+			writer.WriteStringValue(date.ToUnixTimeSeconds().ToString());
+		}
+		public override DateTimeOffset Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+		{
+			var rawValue = reader.GetInt64();
+			return DateTimeOffset.FromUnixTimeSeconds(rawValue);
+		}
+    }
+}
---
+++ tests/RulesExercise.Tests/TemplateManagerTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using RulesExercise.Infrastructure.Persistence;
+using RulesExercise.Infrastructure.Templates;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using Xunit;
+
+namespace RulesExercise.Tests
+{
+    public class TemplateManagerTests
+    {
+        [Theory]
+        [InlineData("{{ Value }} here", "{\"Value\": \"I am\"}", "I am here")]
+        public void Some(string template, string rawDictionary, string expectedResult)
+        {
+            // Arrange
+            var t = new DbContextOptions<ApplicationDbContext>();
+            var fake_context = new Mock<ApplicationDbContext>(t);
+            var dict = JsonSerializer.Deserialize<Dictionary<string, string>>(rawDictionary);
+            var preparedDict = dict.ToDictionary(x => x.Key, x => (object)x.Value);
+            Assert.NotNull(dict);
+
+            var templateManager = new TemplateManager(fake_context.Object);
+
+            // Act
+            
+            var result = templateManager.FormatFromTemplate(template, preparedDict);
+            
+            // Assert
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
